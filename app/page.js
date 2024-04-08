dfe5d60c9913276be3b96fcf15445e7d8d7a708c
stan <nzegwustanley@gmail.com>
2024-04-08 19:24:36 +0100

    new commit

--- app/page.js
+++ app/page.js
@@ -4,14 +4,20 @@ import RightSidebar from "./container/RightSidebar";
 
 export default function Home() {
   return (
-    <main className="flex justify-between bg-gray-500 text-white">
+    <main className="flex justify-end bg-[#090330] text-white">
       <LeftSidebar/>
       <div className="relative w-[83%]">
-        <div className="fixed w-full h-16 z-50 bg-gray-500">
-            TopBar
-        </div>
+        <nav className="fixed h-16 z-50 bg-[#090330] w-[83%]">
+            <div className="w-full h-full flex items-center justify-end pr-16">
+              <div className="flex gap-2 items-center">
+                <img src="/assets/icons/navIcon1.png" alt="" className="w-[24px] h-[24px]"/>
+                <img src="/assets/icons/navIcon2.png" alt="" className="w-[24px] h-[24px]"/>
+                <img src="/assets/icons/upbar.png" alt="" className="w-[24px] h-[24px]"/>
+              </div>
+            </div>
+        </nav>
         <div className="px-4 mt-16">
-        <div className="w-full flex rounded-3xl bg-[#09032F] p-6">
+        <div className="w-full flex rounded-3xl bg-[#09072A] p-6">
           <MainContent />
           <RightSidebar />
         </div>
