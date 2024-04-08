ec0fca8fa08390e97c11d2515dabe7e4f2fe7f2e
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-04-08 19:38:28 +0100

    new commit

--- app/container/LeftSidebar.jsx
+++ app/container/LeftSidebar.jsx
@@ -2,7 +2,7 @@ const LeftSidebar = () => {
   return (
     <div className="fixed top-0 left-0 h-full w-[17%] bg-[#131623] overflow-y-auto z-[500] p-4 py-8">
       <div className="flex justify-center">
-        <img src="/assets/icons/birbleAILogo.png" alt="birble_logo" className=""/>
+        <img src="/assets/icons/birbleAILogo.png" alt="birble_logo" className="" />
       </div>
 
       <div className="flex flex-col gap-6 mt-10">
@@ -12,15 +12,15 @@ const LeftSidebar = () => {
           <span className="pl-8 font-semibold">AI Generator</span>
           <div className="space-y-4">
             {[
-              {iconPath:'/assets/icons/conversationIcon.png',name:'conversation'},
-              {iconPath:'/assets/icons/imageIcon.png',name:'Image creation'},
-              {iconPath:'/assets/icons/music.png',name:'musc creation'},
-              {iconPath:'/assets/icons/videoIcon.png',name:'video generation'},
-              {iconPath:'/assets/icons/codeIcon.png',name:'code generation'},
-              {iconPath:'/assets/icons/TextVoiceIcon.png',name:'text to voice'}
-            ].map(({iconPath,name},index) => (
-              <div className="flex gap-3 items-center pl-16" key={index}>
-                <img src={iconPath} alt="icon" className='w-[10px] h-[10px]'/>
+              { iconPath: "/assets/icons/conversationIcon.png", name: "conversation" },
+              { iconPath: "/assets/icons/imageIcon.png", name: "Image creation" },
+              { iconPath: "/assets/icons/music.png", name: "musc creation" },
+              { iconPath: "/assets/icons/videoIcon.png", name: "video generation" },
+              { iconPath: "/assets/icons/codeIcon.png", name: "code generation" },
+              { iconPath: "/assets/icons/TextVoiceIcon.png", name: "text to voice" },
+            ].map(({ iconPath, name }, index) => (
+              <div className="flex gap-3 items-center pl-10" key={index}>
+                <img src={iconPath} alt="icon" className="w-[10px] h-[10px]" />
                 <p className="capitalize text-[#B2B2B2]">{name}</p>
               </div>
             ))}
@@ -29,17 +29,17 @@ const LeftSidebar = () => {
 
         <span className="pl-8 font-medium">Collections</span>
         <span className="pl-8 font-medium">Token Sale</span>
-        <img src="/astronaut.png" alt="astronaut" className="my-4"/>
+        <img src="/astronaut.png" alt="astronaut" className="my-4" />
         <span className="pl-8 font-medium">Profiles & Settings</span>
         <span className="pl-8 font-medium">FAQ & Help</span>
         <span className="pl-8 font-medium">Logout</span>
 
         <div className="flex gap-3 items-center mt-4">
-            <img src="/person.png" alt="person" className="rounded-2xl w-[65px] h-[65px]"/>
-            <div className="flex flex-col">
-              <span className="font-semibold ">Mima Smith</span>
-              <span className="text-[14px] text-[#B2B2B2]">Designer</span>
-            </div>
+          <img src="/person.png" alt="person" className="rounded-2xl w-[65px] h-[65px]" />
+          <div className="flex flex-col">
+            <span className="font-semibold ">Mima Smith</span>
+            <span className="text-[14px] text-[#B2B2B2]">Designer</span>
+          </div>
         </div>
       </div>
     </div>
